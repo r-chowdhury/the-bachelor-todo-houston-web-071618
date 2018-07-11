be28e01c7bf637b34d5a38344d896680fec3df95
ryhan.chowdhury3@gmail.com <ryhan.chowdhury3@gmail.com>
2018-07-11 15:49:13 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,6 +1,6 @@
 def get_first_name_of_season_winner(data, season)
   # code here
-  data[season].each do |people|
+  data[season].each do |contestants|
     people.each do |k,v|
       if v == "Winner"            
         full_name = people["name"]
